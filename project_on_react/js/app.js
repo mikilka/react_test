9be99db1482f6ce52b212a430fbbe6452d54b373
Melnichenko Mykola <mykola.melnichenko@gmail.com>
2016-10-10 01:47:16 +0300

    on click event

--- project_on_react/js/app.js
+++ project_on_react/js/app.js
@@ -1,34 +1,83 @@
 var my_news = [
-	{
-		author: 'Саша Печкин',
-		text: 'В четверг, четвертого числа...'
-	},
-	{
-		author: 'Просто Вася',
-		text: 'Считаю, что $ должен стоить 35 рублей!'
-	},
-	{
-		author: 'Гость',
-		text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
-	}
+    {
+        author: 'Саша Печкин',
+        text: 'В четчерг, четвертого числа...',
+        bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+    },
+    {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
+    },
+    {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+    }
 ];
 
+var Article = React.createClass({
+    propTypes: {
+        data: React.PropTypes.shape({
+            author: React.PropTypes.string.isRequired,
+            text: React.PropTypes.string.isRequired,
+            bigText: React.PropTypes.string.isRequired
+        })
+    },
+    getInitialState: function() {
+        return {
+            visible: false
+        };
+    },
+    readmoreClick: function(e) {
+        e.preventDefault();
+        this.setState({visible: true});
+    },
+
+    render: function() {
+        var author = this.props.data.author,
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
+
+        return (
+            <div className='article'>
+                <p className='news__author'>{author}:</p>
+                <p className='news__text'>{text}</p>
+
+                {/* для ссылки readmore: не показывай ссылку, если visible === true */}
+                <a href="#"
+                onClick={this.readmoreClick}
+                className={'news__readmore ' + (visible ? 'none': '')}>
+                Подробнее
+                </a>
+
+                {/* для большо текста: не показывай текст, если visible === false */}
+                <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+
+            </div>
+        )
+    }
+});
+
 var News = React.createClass({
-	render: function() {
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
+    render: function() {
 		var data = this.props.data;
 		var newsTemplate;
-		if (data.length > 0) {
-		newsTemplate = data.map(function(item, index) {
-			return (
-			<div key={index}>
-				<p className="news__author">{item.author}:</p>
-				<p className="news__text">{item.text}</p>
-			</div>
-			)
-		})
-	} else {
-	newsTemplate = <p>К сожалению новостей нет</p>
-	}
+        if (data.length > 0) {
+            newsTemplate = data.map(function(item, index) {
+                return (
+                    <div key={index}>
+                        <Article data={item} />
+                    </div>
+                    )
+            })
+        } else {
+            newsTemplate = <p>К сожалению новостей нет</p>
+        }
 
 
 		return (
@@ -39,24 +88,23 @@ var News = React.createClass({
 		);
 	}
 });
-
-var Comments = React.createClass({
-	render: function() {
-		return (
-			<div className="news">
-				Нет новостей - комментировать нечего!
-			</div>
-		);
-	}
-});
+//
+//var Comments = React.createClass({
+//	render: function() {
+//		return (
+//			<div className="news">
+//				Нет новостей - комментировать нечего!
+//			</div>
+//		);
+//	}
+//});
 
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="app">
-				Всем привет, я компонент App!
+                <h3>Новости</h3>
 				<News data={my_news} /> {/*добавили свойство data */}
-				<Comments />
 			</div>
 		);
 	}
